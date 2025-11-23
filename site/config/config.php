6cd3387b783770c3be8bdc655cf9c853e94fb85f
Logan Shimmer <loganherr@gmail.com>
2025-11-22 20:17:38 -0600

    debug: Enable debug mode to see actual PHP errors

--- site/config/config.php
+++ site/config/config.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+  'debug' => true,  // TEMPORARY: Enable to see errors
+
   'url' => $_SERVER['SERVER_NAME'] === 'localhost'
     ? 'http://localhost:8000'
     : 'https://shimmerlabs.co',
